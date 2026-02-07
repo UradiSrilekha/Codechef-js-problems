55f9c86e85c4d5b5740125076137b808e600d449
UradiSrilekha <srilekhauradi@gmail.com>
2026-02-07 07:43:06 +0530

    Factorial of  a number

---
+++ 6.js
@@ -0,0 +1,14 @@
+// Solution as follows
+
+function printFactorial(n) {
+  var factorial = 1; 
+  for(var i = 1; i <= n; i++) {
+     factorial *= i;
+  }
+  console.log("The factorial of the given number is:", factorial);
+}
+
+function solve(n) {
+   printFactorial(n) ;
+}
+ 
