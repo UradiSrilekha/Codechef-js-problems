7f676f12824abcd5db4e54094774593bca151f0d
UradiSrilekha <srilekhauradi@gmail.com>
2026-02-08 12:27:11 +0530

    Implement basic arithmetic operations from stdin
    Read two numbers from stdin and log their sum and difference.

---
+++ 7.js
@@ -0,0 +1,8 @@
+process.stdin.setEncoding('utf8');
+process.stdin.on('data', function(input) {
+    const nums = input.trim().split('\n');
+    let x = parseInt(nums[0]);
+    let y = parseInt(nums[1]);
+    console.log(x+y);
+    console.log(x-y);
+});
